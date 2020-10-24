271b3ca27f9031ddb204e0995630558cfd54cc7b
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-10-24 13:37:00 +0800

    为nn.Sequential增加add_module方法

--- nptorch/nn/modules/container.py
+++ nptorch/nn/modules/container.py
@@ -11,6 +11,11 @@ class Sequential(Module):
     def __len__(self):
         return self.length
 
+    def add_module(self, name, module):
+        if not isinstance(module, Module):
+            raise TypeError(f'{type(module)} is not a Module subclass')
+        self.__setattr__(str(name), module)
+
     def forward(self, x):
         for module in self.children():
             x = module(x)
