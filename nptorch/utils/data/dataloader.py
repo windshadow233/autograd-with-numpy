ebc3253dbd8b99c2ac9a446dd20cbccc238ba956
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-10-26 14:26:38 +0800

    Update dataloader.py

--- nptorch/utils/data/dataloader.py
+++ nptorch/utils/data/dataloader.py
@@ -4,7 +4,14 @@ import math
 
 
 def default_collate_fn(all_data: list):
+    """
+    默认的数据打包函数,为数据生成一个新的维度(第0维),在该维度将数据矩阵进行堆叠
+    @param all_data: 一个列表,元素类型为tuple,tuple中的元素为
+    @return: 按以上规则生成的数据列表
+    """
     data_list = []
+    if not isinstance(all_data[0]):
+        all_data = [(data, ) for data in all_data]
     data_length = len(all_data[0])
     for i in range(data_length):
         data_list.append(array(np.r_[[data[i].data for data in all_data]]))
