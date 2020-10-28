307b4b6c25342fd908987c5bf7f82aac1af60a93
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-10-28 13:51:17 +0800

    做了一些小的修改

--- mnist.py
+++ mnist.py
@@ -25,9 +25,9 @@ class MNISTDataset(Dataset):
         return trans(self.data[item]), self.label[item]
 
 
-class CNN(nn.Module):
+class LeNet(nn.Module):
     def __init__(self):
-        super(CNN, self).__init__()
+        super(LeNet, self).__init__()
         self.layers1 = nn.Sequential(
             nn.Conv2d(1, 16, 5),
             nn.MaxPool2d(2),
@@ -52,12 +52,13 @@ class CNN(nn.Module):
 
 
 def test_model(model, test_loader: DataLoader):
-    model.eval()
-    count = 0
-    for d, lb in test_loader:
-        p = model(d).argmax(-1)
-        count += (p == lb).float().sum()
-    return count.item() / len(test_loader.dataset)
+    with nptorch.no_grad():
+        model.eval()
+        count = 0
+        for d, lb in test_loader:
+            p = model(d).argmax(-1)
+            count += (p == lb).float().sum()
+        return count.item() / len(test_loader.dataset)
 
 
 def load_mnist(img_path, label_path):
@@ -76,29 +77,29 @@ test_set = MNISTDataset('mnist/MNIST/raw/t10k-images-idx3-ubyte', 'mnist/MNIST/r
 train_loader = DataLoader(train_set, batch_size=64)
 test_loader = DataLoader(test_set, batch_size=128)
 
-cnn = CNN()
-optimizer = SGD(cnn.parameters(), lr=1e-1, momentum=0.7)
+model = LeNet()
+optimizer = SGD(model.parameters(), lr=1e-1, momentum=0.7)
 loss_fcn = nn.CrossEntropyLoss()
 
 for i in tqdm(range(5)):
     count = 0
     for n, data in enumerate(train_loader, 1):
-        cnn.train()
+        model.train()
         d, lb = data
         count += len(d)
         print(n)
         print(count)
         print('标签:', lb)
-        y_hat = cnn(d)
+        y_hat = model(d)
         loss = loss_fcn(y_hat, lb)
         loss.backward()
         optimizer.step()
-        cnn.eval()
-        p = cnn(d).argmax(-1)
+        model.eval()
+        p = model(d).argmax(-1)
         print('优化后预测:', p)
         optimizer.zero_grad()
         print(f'优化后的准确比率:{(p == lb).float().sum().item() / len(d)}')
 
 
-print(f'测试集准确率{test_model(cnn, test_loader)}')
-print(f'训练集准确率{test_model(cnn, train_loader)}')
+print(f'测试集准确率{test_model(model, test_loader)}')
+print(f'训练集准确率{test_model(model, train_loader)}')
