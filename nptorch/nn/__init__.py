e5ff1ffb025bba0dd2cfc0398dcd946024465116
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-10-31 01:47:41 +0800

    增加了Mish激活函数

--- nptorch/nn/__init__.py
+++ nptorch/nn/__init__.py
@@ -1,3 +1,4 @@
-from .modules import Module, Linear, Softmax, Sigmoid, ReLU, Tanh, LeakyReLU, ELU, CrossEntropyLoss, MSELoss, NLLLoss, \
-    Dropout, Dropout2d, Sequential, Conv2d, MaxPool2d, MeanPool2d, BatchNorm2d, MeanPool1d, MaxPool1d, Softplus, RNN
+from .modules import Module, Linear, Softmax, Sigmoid, Softplus, ReLU, Tanh, LeakyReLU, ELU, Mish, CrossEntropyLoss, \
+    MSELoss, NLLLoss, Dropout, Dropout2d, Sequential, Conv2d, MaxPool2d, MeanPool2d, BatchNorm2d, MeanPool1d, \
+    MaxPool1d, Softplus, RNN
 from .parameter import Parameter
