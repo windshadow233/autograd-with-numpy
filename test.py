76410f30f27ae0871a968f308a80d3a8b9dba099
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-11-24 23:48:29 +0800

    Update test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import nptorch
-from nptorch import nn
-
-nptorch.random.seed(0)
-x = nptorch.random.randint(0, 10, size=(2,3,6), dtype=float, requires_grad=True)
-conv = nn.MaxPool1d(kernel_size=3, stride=1)
-y = conv(x)
-y.sum().backward()
