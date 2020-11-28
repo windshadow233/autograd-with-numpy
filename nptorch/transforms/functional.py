b0808c85395b3836cc3c2abcd562c5336fb1a0a7
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-11-29 00:01:30 +0800

    修改了to_tensor

--- nptorch/transforms/functional.py
+++ nptorch/transforms/functional.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+import re
 from ..tensor import Tensor
 from ..autograd import no_grad
 
@@ -22,25 +23,24 @@ def _is_tensor(img):
 
 @no_grad()
 def to_tensor(img):
-    if not (_is_tensor(img) or _is_numpy(img) or _is_pil_img(img)):
-        raise TypeError(f'img should be Tensor, PIL Image or ndarray. Got {type(img)}')
-    if isinstance(img, (np.ndarray, Tensor)):
+    if not (_is_numpy(img) or _is_pil_img(img)):
+        raise TypeError(f'img should be PIL Image or ndarray. Got {type(img)}')
+    if isinstance(img, np.ndarray):
         assert img.ndim in {2, 3}, f'img should be 2 or 3 dimensional. Got {img.ndim} dimensions.'
         if img.ndim == 2:
             img = img[:, :, None]
         img = img.transpose((2, 0, 1))
-        if np.max(img) <= 1.:
-            return Tensor(img, dtype=np.float32)
-        else:
-            return Tensor(img / 255., dtype=np.float32)
+        return Tensor(img, dtype=np.float32)
+    array = np.array(img)
+    max_value = 2 ** int(re.search('\d+', array.dtype.name).group())
     if img.mode == '1':
-        img = np.array(img)[:, :, None]
+        array = array[:, :, None]
     elif img.mode in {'L', 'I', 'F', 'P'}:
-        img = np.array(img)[:, :, None] / 255.
+        array = array[:, :, None] / (max_value - 1)
     elif img.mode in {'RGB', 'RGBA', 'CMYK', 'YCbCr'}:
-        img = np.array(img) / 255.
-    img = img.transpose((2, 0, 1))
-    return Tensor(img, dtype=np.float32)
+        array = array / (max_value - 1)
+    array = array.transpose((2, 0, 1))
+    return Tensor(array, dtype=np.float32)
 
 
 def resize(img, size):
