e770cfeba6d6983cfb1eca530d90996f8e5e6743
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2021-08-05 21:20:20 +0800

    Update __init__.py

--- nptorch/nn/__init__.py
+++ nptorch/nn/__init__.py
@@ -1,5 +1,5 @@
 from .modules import Module, Sequential, ModuleList, Identity, Linear, Softmax, Dropout, Dropout2d, Conv2d, Conv1d,\
-    MaxPool2d, MeanPool2d, BatchNorm2d, BatchNorm1d, MeanPool1d, MaxPool1d, RNN, LSTM, GRU, Sigmoid, Softplus, ReLU,\
+    MaxPool2d, AvgPool2d, BatchNorm2d, BatchNorm1d, AvgPool1d, MaxPool1d, RNN, LSTM, GRU, Sigmoid, Softplus, ReLU,\
     Tanh, LeakyReLU, ELU, CrossEntropyLoss, MSELoss, NLLLoss, BCELoss, PairwiseDistance, CosineSimilarity, Embedding
 from .parameter import Parameter
 from . import utils
